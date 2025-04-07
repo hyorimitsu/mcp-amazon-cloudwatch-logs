997066c649e7e6a1d176dcf2a6b10890ef891710
Hiroki Yorimitsu <52403055+hyorimitsu@users.noreply.github.com>
2025-04-07 04:07:38 +0000

    feat: add descriptions to log group schemas for improved clarity

--- src/operations/schemas/groups.ts
+++ src/operations/schemas/groups.ts
@@ -8,22 +8,47 @@ import { typeSafeSchema } from '../../lib/zod/helper.ts'
 
 export const CreateLogGroupRequestSchema = typeSafeSchema<CreateLogGroupCommandInput>()(
   z.object({
-    logGroupName: z.string(),
-    kmsKeyId: z.string().optional(),
-    tags: z.record(z.string(), z.string()).optional(),
-    logGroupClass: z.nativeEnum(LogGroupClass).optional(),
+    logGroupName: z.string().describe('A name for the log group.'),
+    kmsKeyId: z
+      .string()
+      .optional()
+      .describe('The Amazon Resource Name (ARN) of the KMS key to use when encrypting log data.'),
+    tags: z
+      .record(z.string(), z.string())
+      .optional()
+      .describe('The key-value pairs to use for the tags.'),
+    logGroupClass: z
+      .nativeEnum(LogGroupClass)
+      .optional()
+      .describe('Use this parameter to specify the log group class for this log group.'),
   }),
 )
 
 export const CreateLogGroupResponseSchema = typeSafeSchema<CreateLogGroupCommandOutput>()(
   z.object({
-    $metadata: z.object({
-      httpStatusCode: z.number(),
-      requestId: z.string(),
-      extendedRequestId: z.string(),
-      cfId: z.string(),
-      attempts: z.number(),
-      totalRetryDelay: z.number(),
-    }),
+    $metadata: z
+      .object({
+        httpStatusCode: z
+          .number()
+          .describe('The status code of the last HTTP response received for this operation.'),
+        requestId: z
+          .string()
+          .describe(
+            'A unique identifier for the last request sent for this operation. Often requested by AWS service teams to aid in debugging.',
+          ),
+        extendedRequestId: z
+          .string()
+          .describe('A secondary identifier for the last request sent. Used for debugging.'),
+        cfId: z
+          .string()
+          .describe('A tertiary identifier for the last request sent. Used for debugging.'),
+        attempts: z.number().describe('The number of times this operation was attempted.'),
+        totalRetryDelay: z
+          .number()
+          .describe(
+            'The total amount of time (in milliseconds) that was spent waiting between retry attempts.',
+          ),
+      })
+      .describe('Metadata pertaining to this request.'),
   }),
 )
